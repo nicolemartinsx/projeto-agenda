272281e6e5aa9f7ca70e255a1e9d54e050a7c076
Nicole Martins <nicolemartins@alunos.utfpr.edu.br>
2024-06-26 09:21:23 -0300

    ✨feat: importar compromissos de arquivo .csv

--- src/gui/CompromissoWindow.java
+++ src/gui/CompromissoWindow.java
@@ -3,11 +3,15 @@ package gui;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,10 +29,9 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
 
-import org.w3c.dom.Text;
-
 import entities.Agenda;
 import entities.Compromisso;
+import entities.Sessao;
 import service.CompromissoService;
 
 public class CompromissoWindow extends JFrame {
@@ -113,44 +116,75 @@ public class CompromissoWindow extends JFrame {
 		}
 		JOptionPane.showMessageDialog(null, convidados, "Convidados", JOptionPane.DEFAULT_OPTION);
 	}
-	
+
 	private void importarCompromissos() {
-		
+		try {
+
+			JFileChooser seletor = new JFileChooser();
+			FileNameExtensionFilter filtro = new FileNameExtensionFilter("Documento CSV (*.csv)", "csv");
+			seletor.setFileFilter(filtro);
+			int retorno = seletor.showOpenDialog(null);
+
+			if (retorno == JFileChooser.APPROVE_OPTION) {
+				File csvFile = seletor.getSelectedFile();
+
+				BufferedReader br = new BufferedReader(new FileReader(csvFile));
+				String linha;
+				br.readLine();
+
+				while ((linha = br.readLine()) != null) {
+					Compromisso compromisso = new Compromisso();
+
+					String[] valores = linha.split(",");
+
+					compromisso.setTitulo(valores[0]);
+					compromisso.setDescricao(valores[1]);
+					compromisso.setDataInicio(Timestamp.valueOf(valores[2]));
+					compromisso.setDataTermino(Timestamp.valueOf(valores[3]));
+					compromisso.setLocal(valores[4]);
+					compromisso.setAgenda(agenda);
+
+					compromissoService.cadastrarCompromisso(compromisso, Sessao.getUsuario().getIdUsuario());
+				}
+				JOptionPane.showMessageDialog(this, "Compromissos salvos!", "Sucesso!",
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+
+			buscarCompromissos();
+
+		} catch (SQLException | IOException e) {
+			JOptionPane.showMessageDialog(this, "Erro ao importar compromissos", "Erro", JOptionPane.ERROR_MESSAGE);
+		}
+
 	}
 
 	private void exportarCompromissos() {
 		try {
-			
+
 			JFileChooser fileChooser = new JFileChooser();
 			fileChooser.setDialogTitle("Salvar Compromissos em CSV");
-			fileChooser.setSelectedFile(new File("Agenda"+agenda.getNome()+".csv"));
+			fileChooser.setSelectedFile(new File("Compromissos" + ".csv"));
 			fileChooser.setFileFilter(new FileNameExtensionFilter("Arquivo .CSV", "csv", "text"));
-			
+
 			int userSelection = fileChooser.showSaveDialog(null);
 			if (userSelection == JFileChooser.APPROVE_OPTION) {
 				File fileToSave = fileChooser.getSelectedFile();
 
-				BufferedWriter writer = new BufferedWriter(new FileWriter(fileToSave));		
-				writer.write("ID Compromisso,Título,Descrição,Data Início,Data Término,Local,Agenda ID,Agenda Nome,Agenda Descrição\n");
+				BufferedWriter writer = new BufferedWriter(new FileWriter(fileToSave));
+				writer.write("Título,Descrição,Data Início,Data Término,Local,Agenda Nome\n");
 
-                for (Compromisso compromisso : compromissos) {
-                	
-                	String dataInicio = sdf.format(compromisso.getDataInicio());
-    				String dataTermino = sdf.format(compromisso.getDataTermino());
-    				
-                    writer.write(compromisso.getIdCompromisso() + ",");
-                    writer.write(compromisso.getTitulo() + ",");
-                    writer.write(compromisso.getDescricao() + ",");
-                    writer.write(dataInicio + ",");
-                    writer.write(dataTermino + ",");
-                    writer.write(compromisso.getLocal() + ",");
-                    writer.write(agenda.getIdAgenda() + ",");
-                    writer.write(agenda.getNome() + ",");
-                    writer.write(agenda.getDescricao() + ",");
-                    writer.newLine();
-                }
-                
-                writer.close();
+				for (Compromisso compromisso : compromissos) {
+
+					writer.write(compromisso.getTitulo() + ",");
+					writer.write(compromisso.getDescricao() + ",");
+					writer.write(compromisso.getDataInicio() + ",");
+					writer.write(compromisso.getDataTermino() + ",");
+					writer.write(compromisso.getLocal() + ",");
+					writer.write(agenda.getNome() + ",");
+					writer.newLine();
+				}
+
+				writer.close();
 				JOptionPane.showMessageDialog(this, "Compromissos salvos!", "Sucesso!",
 						JOptionPane.INFORMATION_MESSAGE);
 			}
